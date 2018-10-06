d2784d0eb3cc4936c4347c2be7323b442e4638e5
Bryan Didur <bryandidur.dev@gmail.com>
2018-10-06 19:08:43 -0300

    feat: custom json response class

---
+++ app/Http/Responses/JsonResponse.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse as BaseJsonResponse;
+
+class JsonResponse extends BaseJsonResponse
+{
+    /**
+     * Create a new response instance.
+     *
+     * @param  array|null  $data
+     * @param  int|null    $status
+     * @param  array|null  $headers
+     * @param  int|null    $options
+     * @return void
+     */
+    public function __construct(?array $data = [], int $status = Response::HTTP_OK, array $headers = [], int $options = 0)
+    {
+        parent::__construct($this->handleData($data), $this->handleStatusCode($data, $status), $headers, $options);
+    }
+
+    /**
+     * Make a new class object statically.
+     *
+     * @see self::__construct()
+     */
+    public static function make(?array $data = [], int $status = Response::HTTP_OK, array $headers = [], int $options = 0)
+    {
+        return new self($data, $status, $headers, $options);
+    }
+
+    /**
+     * Handle the response data.
+     *
+     * @param  array  $data
+     * @return array
+     */
+    public function handleData(?array $data): array
+    {
+        return compact('data');
+    }
+
+    /**
+     * Handle the response status code.
+     *
+     * @param  array  $data
+     * @param  int    $status
+     * @return int
+     */
+    protected function handleStatusCode(?array $data, int $status): int
+    {
+        if ($data === null) {
+            return Response::HTTP_NOT_FOUND;
+        }
+
+        return $status;
+    }
+}
