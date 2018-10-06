599961fcf2ce4b24aaf2b67fa723d9efd17735be
Bryan Didur <bryandidur.dev@gmail.com>
2018-10-06 19:06:17 -0300

    feat: middleware to force a json response from the framework

---
+++ app/Http/Middleware/SetJsonRequestHeaders.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SetJsonRequestHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+        $request->headers->set('X-Requested-With', 'XMLHttpRequest');
+
+        return $next($request);
+    }
+}
