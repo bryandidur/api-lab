60523ce19bcc8b8ec07200140d3465798c3f006a
Bryan Didur <bryandidur.dev@gmail.com>
2018-11-10 14:38:00 -0200

    fix: user validations

--- app/Validations/UserValidation.php
+++ app/Validations/UserValidation.php
@@ -29,7 +29,7 @@ trait UserValidation
         return ValidatorFactory::make($data, [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            'password' => 'required|string|min:6',
         ]);
     }
 
